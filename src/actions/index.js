f5d7d41c49197cd1a450e69029651a815490e5f0
fultonbryson <dev.brysonfulton@gmail.com>
2021-10-26 20:05:15 -0600

    modified fetchPostsWithQuery actionCreator to take in an optional callback

--- src/actions/index.js
+++ src/actions/index.js
@@ -15,15 +15,19 @@ export function fetchRecentPosts() {
   };
 }
 
-export function fetchPostsWithQuery(query) {
+export function fetchPostsWithQuery(query, callback) {
   return function (dispatch) {
-    const getreq = `https://api.dailysmarty.com/search?q=${query}`;
-    axios.get(getreq).then((response) => {
-      console.log("query: ", response.data.posts);
-      dispatch({
-        type: SET_RESULTS_POSTS,
-        payload: response.data.posts,
+    axios
+      .get(`https://api.dailysmarty.com/search?q=${query}`)
+      .then((response) => {
+        console.log("query: ", response.data.posts);
+        dispatch({
+          type: SET_RESULTS_POSTS,
+          payload: response.data.posts,
+        });
+        if (callback) {
+          callback();
+        }
       });
-    });
   };
 }
