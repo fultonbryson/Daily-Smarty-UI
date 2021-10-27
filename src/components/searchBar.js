768e72fa3ba211fb4964bc055ca6cf72e9357f6a
fultonbryson <dev.brysonfulton@gmail.com>
2021-10-27 00:16:01 -0600

    added styles to searchBar.js

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -20,7 +20,10 @@ class SearchBar extends Component {
       <form
         className='search-bar'
         onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
-        <Field name='query' component={this.renderInput} />
+        <div className='search-bar__wrapper'>
+          <Field name='query' component={this.renderInput} />
+          <p>Press Return to search</p>
+        </div>
       </form>
     );
   }
